d0ab45ef9cd255a3f09809c91e3878d1e98c843e
Jonatas Laguna <jonataslaguna.js@gmail.com>
2024-05-02 15:20:26 -0300

    drink page initial commit

---
+++ src/pages/Drink/index.tsx
@@ -0,0 +1,14 @@
+import styles from './drink.module.css';
+
+function Drink() {
+  return (
+    <div className={ styles.drinkContainer }>
+      <h1>Drinks</h1>
+      <div className={ styles.cardGrid }>
+
+      </div>
+    </div>
+  );
+}
+
+export default Drink; 
