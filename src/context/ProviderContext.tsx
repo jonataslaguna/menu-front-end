bb92f18e14e1492397c1178f2bb07a15695bcdba
Jonatas Laguna <jonataslaguna.js@gmail.com>
2024-05-02 15:19:54 -0300

    implemented Context API

---
+++ src/context/ProviderContext.tsx
@@ -0,0 +1,15 @@
+import MenuContext from "./MenuContext";
+
+type MenuProviderProps = {
+  children: React.ReactNode;
+};
+
+function MenuProvider({ children }: MenuProviderProps) {
+  return (
+    <MenuContext.Provider value={{}}>
+      {children}
+    </MenuContext.Provider>   
+  );
+}
+
+export default MenuProvider;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom';
+import MenuProvider from './context/ProviderContext.ts';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <MenuProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </MenuProvider>
 )
