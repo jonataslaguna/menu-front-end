4ca40bfdb16985f41d8421cb77e283e3aecabd91
Jonatas Laguna <jonataslaguna.js@gmail.com>
2024-05-02 15:18:37 -0300

    food page initial commit

---
+++ src/pages/Food/index.tsx
@@ -0,0 +1,14 @@
+import styles from './food.module.css';
+
+function Food() {
+  return (
+    <div className={ styles.foodContainer }>
+      <h1>Foods</h1>
+      <div className={ styles.cardGrid }>
+
+      </div>
+    </div>
+  );
+}
+
+export default Food; 
