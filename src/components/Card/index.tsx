85b99a3b29ba50c0e9a692f72a6d3997810ee142
Jonatas Laguna <jonataslaguna.js@gmail.com>
2024-05-02 15:19:02 -0300

    card initial commit

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,13 @@
+import styles from './card.module.css';
+
+function Card() {
+  return (
+    <div className={ styles.card }>
+      <img src="" alt="" />
+      <h2></h2>
+      <p></p>
+    </div>
+  )
+}
+
+export default Card;
