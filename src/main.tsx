a5b8ac5924c7415af03f96d48c0becce0bb501cc
Jonatas Laguna <jonataslaguna.js@gmail.com>
2024-05-03 14:50:45 -0300

    config react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom';
-import MenuProvider from './context/ProviderContext.ts';
+import MenuProvider from './context/ProviderContext.tsx';
 import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <MenuProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </MenuProvider>
+  <QueryClientProvider client={queryClient}>
+    <MenuProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+   </MenuProvider>
+  </QueryClientProvider>
 )
