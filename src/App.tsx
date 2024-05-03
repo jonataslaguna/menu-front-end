ba05b308ff75b751ba4e15ac85a10869f04f59ba
Jonatas Laguna <jonataslaguna.js@gmail.com>
2024-05-03 14:49:58 -0300

    added new route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Food from './pages/Food';
 
 function App() {
   
 
   return (
     <Routes>
-      {<Route path='/' element={<Home />}/>}
+      <Route path='/' element={<Home />}/>
+      <Route path='/food' element={<Food />} />
     </Routes>
   )
 }
