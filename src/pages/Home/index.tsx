40f4b288f28901473e5ba51b6b497a28f8088f05
Jonatas Laguna <jonataslaguna.js@gmail.com>
2024-05-06 15:47:14 -0300

    home page initial style

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,10 +2,31 @@ import styles from './home.module.css';
 
 function Home() {
   return (
-    <div className={ styles.homeContainer }>
-      <h1>Home</h1>
+    <div className={styles.homeContainer}>
+      {/* <h1 className={styles.title}>Welcome to Laguna restaurant</h1>
+      <p className={styles.description}>
+        Discover our menu and enjoy the best dishes and drinks in town
+      </p> */}
+      <div className={styles.selector}>
+        <div className={`${styles.imageContainer} ${styles.drinks}`}>
+          <img
+            className={styles.image}
+            src="./images-home-page/drink-home-image.jpg"
+            alt="beer image"
+          />
+           <p className={styles.overlay}>Drinks</p> 
+        </div>
+        <div className={`${styles.imageContainer} ${styles.foods}`}>
+          <img
+            className={styles.image}
+            src="./images-home-page/food-home-image.jpg"
+            alt="pizza image"
+          />
+            <p className={styles.overlay}>Foods</p> 
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Home;
