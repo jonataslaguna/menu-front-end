d2b76d0185a59caf0c97c6938d075621b9c0d231
Jonatas Laguna <jonataslaguna.js@gmail.com>
2024-05-08 14:17:28 -0300

    implemented back icon

--- src/pages/Food/index.tsx
+++ src/pages/Food/index.tsx
@@ -3,12 +3,18 @@ import styles from './food.module.css';
 import MenuContext from '../../context/MenuContext';
 import Card from '../../components/Card';
 import { MenuDataType } from '../../types/MenuDataType';
+import leftIcon from '../../../public/svg/left-icon.svg';
+import { useNavigate } from 'react-router-dom';
 
 function Food() {
   const { menuFoodData } = useContext(MenuContext);
+  const navigate = useNavigate();
 
   return (
     <div className={ styles.foodContainer }>
+      <div className={ styles.backIcon } onClick={() => navigate("/")}>
+        <img src={leftIcon} alt="back icon" />
+      </div>
       <h1>Foods</h1>
       <div className={ styles.cardGrid }>
         {menuFoodData?.map((food: MenuDataType) => (
